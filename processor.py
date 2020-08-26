12517dc9d0b2edcef871fc095013fc5dbd68fa9c
Carl McMillan <carlmcmillan1@gmail.com>
2020-08-26 23:25:08 +1200

    scalar matrix multiplication

--- processor.py
+++ processor.py
@@ -7,24 +7,13 @@ for _ in range(n_a):
         exit(1)
     A.append(row)
 
-(n_b, m_b) = map(int, input().split())
-B = []
-for _ in range(n_b):
-    row = list(map(int, input().split()))
-    if len(row) != m_b:
-        print("ERROR")
-        exit(1)
-    B.append(row)
+c = int(input())
 
-if n_a != n_b or m_a != m_b:
-    print("ERROR")
-    exit(1)
-
-A_B = []
+c_A = []
 for i in range(n_a):
-    A_B.append([])
+    c_A.append([])
     for j in range(m_a):
-        A_B[i].append(A[i][j] + B[i][j])
+        c_A[i].append(c * A[i][j])
 
 for i in range(n_a):
-    print(" ".join(list(map(str, A_B[i]))))
+    print(" ".join(list(map(str, c_A[i]))))
